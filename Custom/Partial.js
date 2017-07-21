c33d64c8a9cc7979a45f818d6b1be4909f86b985
puneet-ahuja <ahujapuneet92@gmail.com>
2017-07-21 10:13:16 +0530

    Update Partial.js

--- Custom/Partial.js
+++ Custom/Partial.js
@@ -2,7 +2,7 @@ function partial(fn){
 	var start_args = Array.prototype.slice.call(arguments , 1);
 	var currentThis = this;
 	return function(){
-		console.log(start_args.concat(arguments));
+		
 		return fn.apply(currentThis,start_args.concat(Array.prototype.slice.call(arguments)));
 	}
 }
@@ -12,3 +12,17 @@ var sum3 = partial(sum , 1 ,2);
 sum3(1,2,3);
 ******************
 9
+
+
+//********************************
+function partialRight(fn){
+	var start_args = Array.prototype.slice.call(arguments , 1);
+	var currentThis = this;
+	return function(){
+		var finalArgs = Array.prototype.slice.call(arguments).concat(start_args);
+		if(finalArgs.length > fn.length){
+			finalArgs = finalArgs(finalArgs.length - fn.length,finalArgs.length)
+		}
+		return fn.apply(currentThis,finalArgs);
+	}
+}
